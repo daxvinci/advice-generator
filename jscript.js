95a2e0d78fd2ed382c4f12b183a5e479b30eee54
da vinci <84037196+daxvinci@users.noreply.github.com>
2023-12-19 17:48:51 +0100

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,5 +1,6 @@
 var txt = document.querySelector(".txt")
 var id = document.querySelector(".id")
+var circle = document.querySelector(".circle")
 
 
 fetch("https://api.adviceslip.com/advice").then(result => {
@@ -11,3 +12,13 @@ fetch("https://api.adviceslip.com/advice").then(result => {
 }).catch(error => {
     console.log(error)
 })
+circle.addEventListener("click", function(){fetch("https://api.adviceslip.com/advice").then(result => {
+    return result.json()
+}).then(resultdata => {
+    var data = resultdata.slip
+    txt.innerText = data.advice
+    var id2 = data.id
+    id.innerText = "Advice #" + id2
+}).catch(error => {
+    console.log(error)
+})})
